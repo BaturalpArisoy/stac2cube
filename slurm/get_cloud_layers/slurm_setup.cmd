d85c0ebf0b1d20a39d5b12a12511a72fc78fe6c5
Baturalp Arisoy <57949580+BaturalpArisoy@users.noreply.github.com>
2025-12-18 17:50:17 +0100

    slurm text changes

--- slurm/get_cloud_layers/slurm_setup.cmd
+++ slurm/get_cloud_layers/slurm_setup.cmd
@@ -3,7 +3,7 @@
 #SBATCH --output=../log/cloud_masking_output.%j.out
 #SBATCH --error=../log/cloud_masking_output.%j.err
 #SBATCH --mail-type=END
-#SBATCH --mail-user=baturalp.arisoy@uni-wuerzburg.de
+#SBATCH --mail-user=<e-mail>
 #SBATCH --account=pr94no-c
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -16,7 +16,7 @@
 module load micromamba
 
 # Local package
-#micromamba run -n stac2cube python slurm_run.py
+micromamba run -n stac2cube python slurm_run.py
 
 # Shared DSS package
-micromamba run -p /dss/dsstbyfs02/pr94no/pr94no-dss-0001/drylands/envs/stac2cube python slurm_run.py
+#micromamba run -p /dss/.../envs/stac2cube python slurm_run.py
